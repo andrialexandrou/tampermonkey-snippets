7f2ed31dcbb17adbd2d77ddfb5d28dae57a8430a
Andri Alexandrou <andrialexandrou@github.com>
2023-05-12 14:23:23 -0700

    Update unset-focus.user.js

--- unset-focus.user.js
+++ unset-focus.user.js
@@ -2,7 +2,7 @@
 // @name         Unset Focus
 // @namespace    https://github.com/andrialexandrou/tampermonkey-snippets
 // @version      0.1
-// @description  Unsets focus on a page to enable default page keyboard interactions.
+// @description  Unsets focus on a page to enable default page keyboard interactions. Usage: Command + Escape will reset your focus to the body of the current document no matter where you are. Handy for restoring default browser keyboard functionality, exiting textareas, and more.
 // @author       Andri Alexandrou
 // @match        https://**/*
 // @grant        none
