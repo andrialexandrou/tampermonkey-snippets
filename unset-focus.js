fd9e9bf30f47b4612eb97a7cdd01a77f88ba8921
Andri Alexandrou <andrialexandrou@github.com>
2023-05-12 14:06:11 -0700

    Create unset-focus.js

---
+++ unset-focus.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Unset Focus
+// @namespace    https://github.com/andrialexandrou/tampermonkey-snippets
+// @version      0.1
+// @description  Unsets focus on a page to enable default page keyboard interactions.
+// @author       Andri Alexandrou
+// @match        https://**/*
+// @grant        none
+// ==/UserScript==
+
+document.onkeydown= function(e) {
+    'use strict';
+    var evtobj = window.event? event : e
+
+    if (!evtobj.metaKey) return
+    if (evtobj.code !== 'Escape') return
+
+    const body = document.getElementsByTagName('body')[0]
+    body.setAttribute('tabindex', -1)
+    body.focus()
+};
